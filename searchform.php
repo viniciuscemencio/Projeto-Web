4599927b11f400574dc271d2924aacd29850f482
vinicius-cemencio <vinicius.v_i@hotmail.com>
2020-04-01 19:29:01 -0300

    Custom button

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
             <form role="seach" method="get" action="<?php echo home_url('/'); ?>" class="form-inline">
                         <input class="form-control ml-4 mr-2 search-form" placeholder="Pesquisar no site..." type="search" value="<?php echo get_search_query(); ?>" name="s" />
-                        <button class="btn btn-default" type="submit"><img src=<?php echo get_template_directory_uri() . '/img/pesquisar.png'?> alt=""></button>
+                        <button class="btn btn-default bg-white" type="submit"><img src=<?php echo get_template_directory_uri() . '/img/pesquisar.png'?> alt=""></button>
              </form>
